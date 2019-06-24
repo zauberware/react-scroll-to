58fbc4bd19403d8da0d1914dac83d468f7fcc285
Philipp Siegmund <philipp@zauberware.com>
2019-06-24 20:35:06 +0200

    Updated activeElement calculation, added smoothscroll-polyfill for IE

--- src/components/ScrollToProvider.js
+++ src/components/ScrollToProvider.js
@@ -1,6 +1,7 @@
 import React, { Component, Children } from 'react'
 import PropTypes from 'prop-types'
 import { ScrollToContext } from './ScrollToContext'
+import smoothscroll from 'smoothscroll-polyfill'
 
 class ScrollToProvider extends Component { 
   constructor(){
@@ -46,15 +47,25 @@ class ScrollToProvider extends Component {
           //var isTotallyVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight)
           var isPartlyVisible = elemTop < window.innerHeight && elemBottom >= 0
           
-          if (isPartlyVisible) workingArray.push({id: obj.id, top: elemTop})
+          if (isPartlyVisible) workingArray.push({id: obj.id, top: elemTop - 60, bottom: elemBottom})
         }
       })
 
       if (workingArray.length > 0) {
         activeElement = workingArray.reduce((prev, curr) => prev.top < curr.top ? prev : curr)
         closestToMid = workingArray.reduce((prev, curr) => (Math.abs(curr.top - (window.innerHeight/2)) < Math.abs(prev.top - (window.innerHeight/2)) ? curr : prev))
-        //console.info("activeElement: " + activeElement.id + " / closest: " + closestToMid.id )
-        this.toggleClosestElement(activeElement.id)
+        // console.info("activeElement: " + activeElement.id + " / closest: " + closestToMid.id )
+        // console.info(activeElement)
+        // console.info(closestToMid)
+        
+        if(activeElement.top < 0 ){
+          this.toggleClosestElement(activeElement.id)
+        }
+        if(activeElement.bottom < 90 && activeElement.top < 0 && closestToMid.top < 250) {
+          this.toggleClosestElement(closestToMid.id)
+        }
+      } else {
+        this.toggleClosestElement('')
       }
     }
   }
@@ -70,10 +81,16 @@ class ScrollToProvider extends Component {
     let section = (this.refRepo.find(section => section.id === id))
     if (section) {
       const element = section.ref
-      element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      })
+      
+      const originalTop = element.getBoundingClientRect().top
+      if(typeof window !== 'undefined'){
+        window.scrollBy({ top: originalTop - 60, left: 0, behavior: "smooth" });
+      }
+      
+      // element.scrollIntoView({
+      //   behavior: 'smooth',
+      //   block: 'start',
+      // })
     }
   }
 
